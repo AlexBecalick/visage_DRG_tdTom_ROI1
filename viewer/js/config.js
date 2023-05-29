120ca0fbcce1ccd9d54ce300cd3a4aae7547fc51
AlexBecalick <alexander.becalick@crick.ac.uk>
2023-05-30 00:52:49 +0100

    Changed link to git LFS data

--- viewer/js/config.js
+++ viewer/js/config.js
@@ -3,8 +3,9 @@ function config() {
             roi: {"x0": 0, "x1": 9263, "y0": 0, "y1": 9239 },
             zoomLevels: 10, // maximum zoom levels. Leave that at 10.
             tiles: 'https://storage.googleapis.com/ca1-data/img/262144px/{z}/{y}/{x}.jpg_ZZZ',
-            cellData: [{mediaLink: './data/cellData.tsv', size: "539686"}],
-            geneData: [{mediaLink: './data/geneData.tsv', size: "18907205"}],
-            cellBoundaries: [{mediaLink: './data/cellBoundaries.tsv', size: "818103"}],
+            cellData: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/visage_DRG_tdTom_ROI1/main/viewer/data/cellData.tsv?token=ARINDHJGFRGMV6M7TGS4RKTEOVEA4', size: "539686"}],
+            geneData: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/visage_DRG_tdTom_ROI1/main/viewer/data/geneData.tsv?token=ARINDHLUZCTJS5MUWNUMHJTEOVECG', size: "18907205"}],
+            cellBoundaries: [{mediaLink: 'https://media.githubusercontent.com/media/AlexBecalick/visage_DRG_tdTom_ROI1/main/viewer/data/cellBoundaries.tsv?token=ARINDHNM3PLEWT5ZAL7WUPDEOVDY2', size: "818103"}],
             }
         }
+        
